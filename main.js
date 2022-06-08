e189b45e26103ac32a9b106606a2e2cafdfc44af
右究 <aisowe@sina.com>
2022-06-08 16:10:33 +0800

    feat: 基本完成

--- main.js
+++ main.js
@@ -3,17 +3,48 @@ import paragraph from './paragraph'
 
 document.querySelector('#app').innerHTML = paragraph
 
-window.addEventListener('mouseup', () => {
-  const selObj = document.getSelection()
-  const { anchorOffset, focusOffset, anchorNode } = selObj
+function injectMarkWords(selector, callback) {
+  // const selectorElement = document.querySelector(selector)
+  window.addEventListener('mouseup', () => {
+    const flag = 'mark'
+    const selObj = document.getSelection()
 
-  const [start, end] = [anchorOffset, focusOffset].sort()
+    // if not select anything. return.
+    if (selObj.type !== 'Range') return
 
-  const { textContent } = anchorNode
-  console.log(anchorNode)
-  const selStr = textContent.slice(start, end)
-  anchorNode.parentElement.innerHTML = `${textContent.slice(
-    0,
-    start,
-  )}<strong style="color: tomato;">{{${selStr}}}</strong>${textContent.slice(end)}`
+    const { startContainer, endContainer } = selObj.getRangeAt(0)
+
+    // whether select start point & end point in a same text node.
+    if (startContainer !== endContainer || startContainer.nodeName !== '#text') return
+
+    // nested mark is not allowed
+    if (flag in startContainer.parentNode.dataset) return
+
+    const { anchorOffset, focusOffset, anchorNode } = selObj
+    const [start, end] = [anchorOffset, focusOffset].sort()
+
+    const { textContent } = anchorNode
+
+    const prevStr = textContent.slice(0, start)
+    const prevTextNode = document.createTextNode(prevStr)
+
+    const selStr = textContent.slice(start, end)
+    const markedElementNode = document.createElement('strong')
+    markedElementNode.classList.add(flag)
+    markedElementNode.dataset[flag] = ''
+    markedElementNode.innerText = selStr
+
+    const nextStr = textContent.slice(end)
+    const nextTextNode = document.createTextNode(nextStr)
+
+    const fragment = document.createDocumentFragment()
+    fragment.append(prevTextNode, markedElementNode, nextTextNode)
+
+    anchorNode.replaceWith(fragment)
+    console.log(selObj.getRangeAt(0).commonAncestorContainer)
+  })
+}
+
+injectMarkWords('#app', () => {
+  console.log()
 })
